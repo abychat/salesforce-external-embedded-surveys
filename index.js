d3c883493c501381ca34e6e6ed2903e8727c447f
Abhishek Chaturvedi <cabhi83@gmail.com>
2021-05-27 17:13:01 -0700

    Externalizing Survey and UI related config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const PORT = process.env.PORT || 5000;
 
 const BG_FAKE = process.env.BG_FAKE;
 const SURVEY_URL = process.env.SURVEY_URL;
+const BUTTON_CLASS = process.env.BUTTON_CLASS;
+const BUTTON_TEXT = process.env.BUTTON_TEXT;
 
 var express = require("express");
 var path = require("path");
@@ -19,6 +21,8 @@ app.get("/", function (req, res) {
   res.render("index", {
     background: BG_FAKE,
     surveyUrl: SURVEY_URL,
+    buttonClass: BUTTON_CLASS,
+    buttonText: BUTTON_TEXT,
   });
 });
 
