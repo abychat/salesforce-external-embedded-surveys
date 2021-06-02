d2b08822f366985a3d8e10916e821ef66747c0ad
Abhishek Chaturvedi <cabhi83@gmail.com>
2021-06-02 13:17:03 -0700

    Externalize Height for Modal and Iframe

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# To Do
+# Salesforce Embedded Survey Example
 
-The entire Read Me :)
-
+This app provides an easy way to demonstrate how to embed Salesforce Surveys in a third party website using an iFrame.
 # Heroku Button
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+# 
--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const PORT = process.env.PORT || 5000;
-
-const BG_FAKE = process.env.BG_FAKE;
-const SURVEY_URL = process.env.SURVEY_URL;
-const BUTTON_CLASS = process.env.BUTTON_CLASS;
-const BUTTON_TEXT = process.env.BUTTON_TEXT;
-const SURVEY_HAS_CONTACT = process.env.SURVEY_HAS_CONTACT;
-const CONTACT_ID = process.env.CONTACT_ID;
-var express = require("express");
-var path = require("path");
-var app = express();
-var cookieParser = require("cookie-parser");
-
+const express = require("express");
+const path = require("path");
+const app = express();
+const cookieParser = require("cookie-parser");
 const jsforce = require("jsforce");
 const { getToken } = require("sf-jwt-token");
 
@@ -19,6 +10,14 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(__dirname + "/public"));
 app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+const BG_FAKE = process.env.BG_FAKE;
+const SURVEY_URL = process.env.SURVEY_URL;
+const BUTTON_CLASS = process.env.BUTTON_CLASS;
+const BUTTON_TEXT = process.env.BUTTON_TEXT;
+const SURVEY_HAS_CONTACT = process.env.SURVEY_HAS_CONTACT;
+const CONTACT_ID = process.env.CONTACT_ID;
 
 let jwttoken;
 const conn = new jsforce.Connection();
@@ -30,6 +29,8 @@ const initConnection = async () => {
       sub: process.env.USERNAME,
       aud: process.env.LOGIN_URL,
       privateKey: process.env.PRIVATE_KEY,
+      modalHeight: process.env.MODAL_HEIGHT,
+      iframeHeight: process.env.IFRAME_HEIGHT,
     });
 
     conn.initialize({
